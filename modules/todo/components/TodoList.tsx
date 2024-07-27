204cb04cb23ee68bfdbba2cc2f8d7c8fb9c7f899
Mohammed <mohammed.sharraf@sazim.io>
2024-07-28 00:21:49 +0600

    feat(NTD): clear completed tasks

--- modules/todo/components/TodoList.tsx
+++ modules/todo/components/TodoList.tsx
@@ -1,49 +1,55 @@
 import React, { useEffect, useState } from "react";
-import { useAppSelector } from "@/shared/redux/hooks";
+import { useAppSelector, useAppDispatch } from "@/shared/redux/hooks";
 import { Todo } from "../models";
 import { Checkbox } from "@/shared/components/ui/checkbox";
 import { Card, CardContent } from "@/shared/components/ui/card";
+import { toggleTodoCompletion } from "@/shared/redux/reducers/todoSlice";
 import Link from "next/link";
 
 const TodoList: React.FC = () => {
   const [isClient, setIsClient] = useState(false);
   const todos = useAppSelector((state) => state.todos.todos);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
   if (!isClient) {
-    return null;
+    return null; 
   }
+
+  const sortedTodos = [...todos].sort((firstNumber, secondNumber) => Number(firstNumber.isCompleted) - Number(secondNumber.isCompleted));
 
   return (
     <div className="space-y-4">
-      {todos.map((todo: Todo) => (
-        <Link key={todo.id} href={`/todo/${todo.id}`} passHref>
-          <Card className="cursor-pointer mb-4">
-            <CardContent className="flex items-center justify-between p-4">
-              <div className="flex items-center space-x-2">
-                <Checkbox
-                  id={`todo-${todo.id}`}
-                  checked={todo.isCompleted}
-                  disabled
-                />
-                <label
-                  htmlFor={`todo-${todo.id}`}
-                  className={`text-md font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${
-                    todo.isCompleted ? 'line-through text-gray-500' : ''
-                  }`}
-                >
-                  Task: {todo.task}
-                </label>
-              </div>
-            </CardContent>
-          </Card>
-        </Link>
+      {sortedTodos.map((todo: Todo) => (
+        <Card key={todo.id}>
+          <CardContent className="flex items-center justify-between p-4">
+            <div className="flex items-center space-x-2">
+              <Checkbox
+                id={`todo-${todo.id}`}
+                checked={todo.isCompleted}
+                onClick={() => dispatch(toggleTodoCompletion(todo.id))}
+              />
+              <label
+                htmlFor={`todo-${todo.id}`}
+                className={`text-md font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 ${
+                  todo.isCompleted ? 'line-through text-gray-500' : ''
+                }`}
+              >
+                Task: {todo.task}
+              </label>
+            </div>
+            <Link href={`/todo/${todo.id}`} legacyBehavior>
+              <a className="text-blue-500">View Details</a>
+            </Link>
+          </CardContent>
+        </Card>
       ))}
     </div>
   );
 };
 
 export default TodoList;
+
